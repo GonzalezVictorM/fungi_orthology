00cf71ecaa2fb619f2c78ecc6ffadb64a231b3bc
Victor Gonzalez <victor.gonzalezramos@aalto.fi>
2025-09-17 17:58:50 +0300

    added a batch job fix

---
+++ src/launch-iprscan.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#SBATCH --account=project_2015320
+#SBATCH --job-name=launch_iprscan
+#SBATCH --output=local_data/logs/launch_iprscan_%J.out
+#SBATCH --error=local_data/logs/launch_iprscan_%J.stderr
+#SBATCH --time=48:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1 
+#SBATCH --mem-per-cpu=2G
+#SBATCH --partition=small
+
+module load biokit
+
+module load interproscan
+
+chmod +x src/batch-interproscan.sh
+
+src/batch-interproscan.sh
